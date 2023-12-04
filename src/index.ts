3426671676840ef64f3f856b7d6071c95fcfd90b
EstebanPastor <estebanandrespastor@gmail.com>
2023-12-04 17:53:17 -0300

    feat: :sparkles: Add connection to the database and the root route

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express from "express";
-import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import taskRoute from "./routes/task.routes";
+import dbConnection from "./config/db";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+app.use("/", taskRoute);
+
+dbConnection();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
